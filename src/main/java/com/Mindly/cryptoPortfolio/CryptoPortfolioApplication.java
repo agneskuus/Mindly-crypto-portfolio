cdc49ea9dd6679400d7da300710ea38453cd4038
Agnes Kuus <40768986+agneskuus@users.noreply.github.com>
2018-07-15 13:16:51 +0300

    CP-1 - Create Spring Boot app

---
+++ src/main/java/com/Mindly/cryptoPortfolio/CryptoPortfolioApplication.java
@@ -0,0 +1,12 @@
+package com.Mindly.cryptoPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPortfolioApplication.class, args);
+	}
+}
