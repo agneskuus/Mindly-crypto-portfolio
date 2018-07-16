2bd9a7038a390bfb0a5ad631785ba180b721ad3e
Agnes Kuus <40768986+agneskuus@users.noreply.github.com>
2018-07-16 21:44:40 +0300

    CP-7 - Add a method to create a new purchase from the front-end #13

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -5,19 +5,28 @@ new Vue({
     el: "#portfolio-table",
     data: {
         cryptos: [],
-        portfolioItems: []
+        portfolio: [],
+        newPortfolioItem: {}
     },
     methods: {
         async getPortfolioItems(){
             let portfolioItems = await PortfolioProvider.getPortfolioItems()
             if (portfolioItems) {
-                this.portfolioItems = portfolioItems
+                this.portfolio = portfolioItems
             }
         },
         async getCryptos(){
             let cryptos = await CryptoProvider.getCryptos()
             if (cryptos) {
                 this.cryptos = cryptos
+                this.newPortfolioItem.crypto = cryptos[0]
+            }
+        },
+        async createPortfolioItem(){
+            let createdItem = await PortfolioProvider.createPortfolioItem(this.newPortfolioItem)
+            if (createdItem) {
+                this.portfolio.push(createdItem)
+                this.newPortfolioItem = {}
             }
         }
     },
